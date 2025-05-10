67742b9dc18b601228b5c500e46a277d60272f9c
SergioCaMi <sergiocami84@gmail.com>
2025-05-10 10:23:43 +0200

    done Bonus 4

--- app.js
+++ app.js
@@ -63,9 +63,9 @@ const server = http.createServer((req, res) => {
       <h2>Lista de colores de NetMind!</h2>
       <ul>
     `);
-
+//res.write(`<li><a href="/color?variant=${element.variant}">${element.variant}</a></li>`);
     colors.forEach((color) => {
-      res.write(`<li>${color.variant}</li>`);
+      res.write(`<li><a href="/color?variant=${color.variant}">${color.variant}</a></li>`);
     });
 
     res.write(`
