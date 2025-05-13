5ec8ceb3de8fbb5a00b57e81d9eb4987c9216529
SergioCaMi <sergiocami84@gmail.com>
2025-05-13 21:03:58 +0200

    done it 2

---
+++ app-express.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const lodash = require('lodash');
+const port = 3000;
+
+const colors = [
+  { variant: "Vermillion", hex: "#2E191B" },
+  { variant: "Forest", hex: "#0B6623" },
+  { variant: "Navy", hex: "#000080" },
+  { variant: "Crimson", hex: "#DC143C" },
+  { variant: "Sky Blue", hex: "#87CEEB" },
+  { variant: "Lime", hex: "#00FF00" },
+  { variant: "Gold", hex: "#FFD700" },
+  { variant: "Lavender", hex: "#E6E6FA" },
+  { variant: "Tangerine", hex: "#F28500" },
+  { variant: "Magenta", hex: "#FF00FF" },
+  { variant: "Cyan", hex: "#00FFFF" },
+  { variant: "Olive", hex: "#808000" },
+  { variant: "Teal", hex: "#008080" },
+  { variant: "Maroon", hex: "#800000" },
+  { variant: "Coral", hex: "#FF7F50" },
+];
+
+
+app.get('/', (req, res)=>{
+    res.send('<h1>Bienvenido a Express</h1>');
+});
+
+
+app.get('/color', (req, res)=>{
+    const color = lodash.sample(colors);
+    res.send(`<p style="color: ${color.hex};">El color escogido ha sido el ${color.variant}.</p>`);
+});
+
+app.listen(port,()=>{
+    console.log('Servidor escuchando en http://localhost:3000');
+});
+
